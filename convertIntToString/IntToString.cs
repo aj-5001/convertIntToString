c961ff710f73400c82813cd7f3120255771cbb50
aj-5001 <alecjasanovsky@gmail.com>
2018-05-03 15:16:51 -0400

    commit 1

---
+++ convertIntToString/IntToString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertIntToString
+{
+    public class IntToString
+    {
+        static string[] ones = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+        static string[] tens = { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        static string[] hundreds = { "Hundred", "Thousand", "Million" };
+
+        static void Main()
+        {
+
+        }
+
+    }
+}
